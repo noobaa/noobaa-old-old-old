dad69109d0f087fc740b79ba84af80b88633e101
gu <guymguym@gmail.com>
2013-08-11 16:06:26 +0300

    tour first steps

--- star/public/js/tour_guide.js
+++ star/public/js/tour_guide.js
@@ -1,14 +1,14 @@
 $(function() {
 	var nb_tour = new Tour({
-		backdrop: true,
+		debug: true,
 		template: [
 			'<div class="popover tour fntread bglight">',
 			'  <div class="arrow"></div>',
-			// '  <h3 class="popover-title"></h3>',
-			'  <div class="popover-content"></div>',
-			'  <div class="popover-navigation">',
-			'    <button class="btn" data-role="end">Close</button>',
-			'    <div class="btn-group" style="float: right">',
+			'  <h1 class="popover-title fntpaint"></h1>',
+			'  <div class="popover-content fntpaint"></div>',
+			'  <div class="popover-navigation modal-footer">',
+			'    <button class="btn pull-left" data-role="end">Close</button>',
+			'    <div class="btn-group">',
 			'      <button class="btn" data-role="prev">« Prev</button>',
 			'      <button class="btn btn-primary" data-role="next">Next »</button>',
 			'    </div>',
@@ -20,25 +20,60 @@ $(function() {
 	nb_tour.addSteps([{
 		element: "#tour_link", // jQuery selector
 		placement: 'bottom',
-		// reflex: true,
-		// title: "NooBaa",
+		backdrop: true,
+		title: "WELCOME TO NOOBAA !",
 		content: [
-			'<h3>WELCOME TO NOOBAA !</h3>',
-			'<p class="lead">We want to show you around.<br/>',
-			'Press next...<br/>(or use left & right keys)<br/></p>'
+			'<p class="lead">This tour will show you around.</p>',
+			'<p class="lead">It\'s all free, and you\'ll be up and running in a minute.</p>',
+			'<p class="lead">Press Next...</p>',
+			// '<p>* You can also navigate this tour using the left & right arrow keys</p>',
+			// '<p>* This tour is always available using the',
+			// '<i class="icon-info-sign text-info"></i> button at the top</p>'
 		].join('\n')
 	}, {
 		element: "#my_data_link",
 		placement: 'bottom',
-		// reflex: true,
-		// title: "MY DATA",
-		content: '<h3>This is your MY DATA page ... </h3>'
+		backdrop: true,
+		title: "MY DATA",
+		content: [
+			'<p class="lead">This is where you manage your files.</p>'
+		].join('\n')
 	}, {
 		element: "#my_dev_link",
 		placement: 'bottom',
-		// reflex: true,
-		// title: "MY DEVICES",
-		content: '<h3>This is your MY DEVICES page ... </h3>'
+		backdrop: true,
+		title: "MY DEVICES",
+		content: [
+			'<p class="lead">This is where you manage your devices.</p>'
+		].join('\n')
+	}, {
+		element: "#add_dev_btn",
+		placement: 'bottom',
+		reflex: true,
+		path: "/mydevices#/mydevices", // TODO: the #/mydevices is needed due to angular bug
+		title: "Add Device",
+		content: [
+			'<p class="lead">Start co-sharing by installing your first device.</p>'
+		].join('\n')
+	}, {
+		element: "#my_data_link",
+		placement: 'bottom',
+		reflex: true,
+		path: "/mydevices#/mydevices",
+		title: "First device added",
+		content: [
+			'<p class="lead">Once your device is up and running,',
+			'you are co-sharing and enjoying the power of NooBaa\'s crowd-cloud!</p>',
+			'<p class="lead">Lets go and meet your data...</p>'
+		].join('\n')
+	}, {
+		element: "#my_data_link",
+		placement: 'bottom',
+		path: "/mydata#/mydata",
+		title: "MY DATA",
+		content: [
+			'<p class="lead">Let\'s see how to manage your data...</p>'
+		].join('\n')
 	}]);
 
 	$('#tour_link').click(function() {
