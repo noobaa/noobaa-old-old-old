67e5be3870a8e71037375e1a9ac1b43df7d2cd41
Yuval Dimnik <yuval.dimnik@gmail.com>
2013-08-15 02:18:41 +0300

    added quota calculation per user

---
+++ star/providers/user_inodes_test.js
@@ -0,0 +1,63 @@
+/*jslint node: true */
+'use strict';
+
+var mongoose = require('mongoose');
+var User = require('../models/user').User;
+var user_inodes = require('./user_inodes');
+
+
+var db;
+
+process.env.FACEBOOK_APP_ID = '123';
+process.env.FACEBOOK_SECRET = 'sec';
+process.env.FACEBOOK_AUTHORIZED_URL = 'callback';
+process.env.MONGOHQ_URL = 'mongodb://admin:admin@localhost/test';
+
+var rand_fb_id = Math.floor((Math.random() * 100000) + 1);
+
+exports.test_user_inodes = {
+	setUp: function(callback) {
+		try {
+			//db.connection.on('open', function() {
+			mongoose.connection.on('open', function() {
+				console.log('Opened connection');
+				callback();
+			});
+
+			db = mongoose.connect(process.env.MONGOHQ_URL);
+			console.log('Started connection, waiting for it to open');
+		} catch (err) {
+			console.log('Setting up failed:', err.message);
+		}
+	},
+
+	'get yuvals quota': function(test) {
+		User.findOne({
+			'fb.first_name': 'Yuval'
+		}, function(err, user) {
+			if (err) {
+				console.log('error' + err);
+				test.ifError(err);
+				return test.done();
+			}
+			user_inodes.get_user_quota_bytes(user, function(err, result) {
+				console.log(result);
+				test.notEqual(result, 0, 'Quota for user was zero');
+				test.ifError(err);
+				test.done();
+			});
+		});
+	},
+
+	tearDown: function(callback) {
+		console.log('In tearDown');
+		try {
+			console.log('Closing connection');
+			db.disconnect();
+			callback();
+		} catch (err) {
+			console.log('Tearing down failed:', err.message);
+		}
+	},
+
+};
