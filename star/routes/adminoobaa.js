aba35cb8687a86b03cf52cf27294614d812b3153
gu <guymguym@gmail.com>
2013-08-14 20:54:47 +0300

    show devices in adminoobaa

--- star/routes/adminoobaa.js
+++ star/routes/adminoobaa.js
@@ -1,18 +1,33 @@
 /* jshint node:true */
 'use strict';
 
+var _ = require('underscore');
 var async = require('async');
 var common_api = require('./common_api');
 var User = require('../models/user').User;
+var Device = require('../models/device.js').Device;
 
 
 exports.admin_view = function(req, res) {
-	async.waterfall([
-		User.find.bind(User)
-	], function(err, users) {
+	async.parallel({
+		users: User.find.bind(User),
+		devices: Device.find.bind(Device)
+	}, function(err, result) {
 		if (err) {
 			return res.send(500, err);
 		} else {
+			var users = {};
+			_.each(result.users, function(u) {
+				users[u._id] = u;
+			});
+			_.each(result.devices, function(d) {
+				var u = users[d.owner];
+				if (!u) {
+					u = users[d.owner] = {};
+				}
+				u.devices = u.devices || {};
+				u.devices[d._id] = d;
+			});
 			var ctx = common_api.page_context(req);
 			ctx.users = users;
 			return res.render('adminoobaa.html', ctx);
