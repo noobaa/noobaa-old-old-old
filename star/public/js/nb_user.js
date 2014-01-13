cf6856cbba3bd6c9c3952c074714a5de0e6d1ea5
gu <guymguym@gmail.com>
2014-01-13 06:53:12 +0200

    add animations, handle fb status on client

--- star/public/js/nb_user.js
+++ star/public/js/nb_user.js
@@ -77,6 +77,56 @@
 				}
 			}
 
+			$scope.fbme = null;
+
+			function on_fb_state_change(res) {
+				console.log('on_fb_state_change', res);
+				// Here we specify what we do with the response anytime this event occurs. 
+				if (res.status === 'connected') {
+					// The response object is returned with a status field that lets the app know the current
+					// login status of the person. In this case, we're handling the situation where they 
+					// have logged in to the app.
+					// testAPI();
+					FB.api('/me', function(me) {
+						console.log('FBME', me);
+						$scope.fbme = me;
+					});
+				} else if (res.status === 'not_authorized') {
+					// In this case, the person is logged into Facebook, but not into the app, so we call
+					// FB.login() to prompt them to do so. 
+					// In real-life usage, you wouldn't want to immediately prompt someone to login 
+					// like this, for two reasons:
+					// (1) JavaScript created popup windows are blocked by most browsers unless they 
+					// result from direct interaction from people using the app (such as a mouse click)
+					// (2) it is a bad experience to be continually prompted to login upon page load.
+					// FB.login();
+				} else {
+					// In this case, the person is not logged into Facebook, so we call the login() 
+					// function to prompt them to do so. Note that at this stage there is no indication
+					// of whether they are logged into the app. If they aren't then they'll see the Login
+					// dialog right after they log in to Facebook. 
+					// The same caveats as above apply to the FB.login() call here.
+					// FB.login();
+					$scope.fbme = null;
+				}
+			}
+
+			function on_fb_init() {
+				// Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+				// for any authentication related change, such as login, logout or session refresh. This means that
+				// whenever someone who was previously logged out tries to log in again, the correct case below 
+				// will be handled.
+				console.log('on_fb_init');
+				FB.Event.subscribe('auth.authResponseChange', on_fb_state_change);
+				FB.getLoginStatus(on_fb_state_change);
+			}
+
+			if (window.fb_init_complete) {
+				on_fb_init();
+			} else {
+				window.on_fb_init = on_fb_init;
+			}
+
 			$scope.login_facebook = function() {
 				if (false && FB) {
 					FB.login();
