281d78c77cf8e3e34e7836abcca27a7497a46a71
gu <guymguym@gmail.com>
2014-02-12 13:08:48 +0200

    try to fix wmic to use list when needed

--- star/public/js/nb_planet.js
+++ star/public/js/nb_planet.js
@@ -671,12 +671,12 @@
 				return list;
 			}
 
-			function wmic_get_list(topic, callback) {
-				execute_os(WINDOWS.CMD, ['/c', 'wmic', topic, 'get', '/format:list'], wmic_parse_list, callback);
+			function wmic_get_list(topic, op, callback) {
+				execute_os(WINDOWS.CMD, ['/c', 'wmic', topic, op, '/format:list'], wmic_parse_list, callback);
 			}
 
-			function wmic_save_info(topic) {
-				wmic_get_list(topic, set_drives_info_callback('win_wmic_' + topic));
+			function wmic_save_info(topic, op) {
+				wmic_get_list(topic, op, set_drives_info_callback('win_wmic_' + topic + '_'+ op));
 			}
 
 			function table_parse(text) {
@@ -692,10 +692,10 @@
 					if (os.platform() === 'win32') {
 						execute_os(WINDOWS.CMD, ['/c', 'dir', 'c:'],
 							set_drives_info_callback('win_dir_c'));
-						wmic_save_info('csproduct');
-						wmic_save_info('diskdrive');
-						wmic_save_info('logicaldisk');
-						wmic_save_info('volume');
+						wmic_save_info('csproduct', 'get');
+						wmic_save_info('diskdrive', 'list');
+						wmic_save_info('logicaldisk', 'list');
+						wmic_save_info('volume', 'list');
 						/*
 							execute_os(WINDOWS.FSUTIL, ['fsinfo', 'drives'], 
 								set_drives_info_callback('win_fsinfo_drives'));
