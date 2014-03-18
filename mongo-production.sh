592a4b437746a5f5ae752034747683270d8a6c0f
gu <guymguym@gmail.com>
2014-03-18 13:09:07 +0200

    pass params to run db scripts

--- mongo-production.sh
+++ mongo-production.sh
@@ -1,2 +1,2 @@
 PWD="P6b_jijck7ES7p8qUtpIfJfgU_m8Yt9PHWpsSmHhOBna0CJeUFx9oEVSTBfCfwpCD7e95q0LMIvhr-9wYXqPQw"
-mongo -u heroku -p ${PWD} dharma.mongohq.com:10008/app16920415
+mongo -u heroku -p ${PWD} dharma.mongohq.com:10008/app16920415 $*
