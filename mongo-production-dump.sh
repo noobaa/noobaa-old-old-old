ba4107e46f07c675ddf186e98d1d022d188d30c5
gu <guymguym@gmail.com>
2014-03-24 19:22:51 +0200

    moved mongo dumps under folder

--- mongo-production-dump.sh
+++ mongo-production-dump.sh
@@ -1,5 +1,5 @@
 DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-OUTPUT="./noobaa-mongohq-dump-${DATE}"
+OUTPUT="./noobaa-mongohq-dump/noobaa-mongohq-dump-${DATE}"
 PWD="P6b_jijck7ES7p8qUtpIfJfgU_m8Yt9PHWpsSmHhOBna0CJeUFx9oEVSTBfCfwpCD7e95q0LMIvhr-9wYXqPQw"
 
 mongodump --host dharma.mongohq.com --port 10008 --db app16920415 --username heroku --password ${PWD} --out ${OUTPUT} 
