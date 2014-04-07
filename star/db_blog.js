720b44824439e2c48111241e5723b36596d8b6ff
gu <guymguym@gmail.com>
2014-04-08 02:55:34 +0300

    add db script to fetch blog events

---
+++ star/db_blog.js
@@ -0,0 +1,12 @@
+db.trackevents.find({
+	event: /blog/
+}).sort({
+	time: 1
+}).forEach(function(e) {
+	var user = e.user ? e.user.name : e.req.ip;
+	var data = e.data || {};
+	var uid = data.uid ? '<'+data.uid+'>' : '';
+	print(e._id.getTimestamp(),
+		'- [ ' + user + ' ]',
+		e.event, uid, data.name||'');
+});
