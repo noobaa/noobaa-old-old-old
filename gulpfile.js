7f764a7a1d624b1aa683f04f761eaa01983c5fed
gu <guymguym@gmail.com>
2014-04-26 17:42:40 +0300

    incrementalize gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,24 @@
 var gulp = require('gulp');
-var filter = require('gulp-filter');
-var less = require('gulp-less');
-var uglify = require('gulp-uglify');
-var minify_css = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var jshint = require('gulp-jshint');
+var gulp_debug = require('gulp-debug');
+var gulp_size = require('gulp-size');
+var gulp_concat = require('gulp-concat');
+var gulp_cached = require('gulp-cached');
+var gulp_newer = require('gulp-newer');
+var gulp_filter = require('gulp-filter');
+var gulp_less = require('gulp-less');
+var gulp_uglify = require('gulp-uglify');
+var gulp_minify_css = require('gulp-minify-css');
+var gulp_rename = require('gulp-rename');
+var gulp_bower = require('gulp-bower');
+var gulp_ng_template = require('gulp-angular-templatecache');
+var gulp_nodemon = require('gulp-nodemon');
+var gulp_jshint = require('gulp-jshint');
 var jshint_stylish = require('jshint-stylish');
-var bower = require('gulp-bower');
-var browserify = require('gulp-browserify');
-var ng_template_cache = require('gulp-angular-templatecache');
-var nodemon = require('gulp-nodemon');
-var es = require('event-stream');
-var gconcat = require('gulp-concat');
-var ngmin = require('gulp-ngmin');
+var vinyl_buffer = require('vinyl-buffer');
+var vinyl_source_stream = require('vinyl-source-stream');
+var browserify = require('browserify');
+var event_stream = require('event-stream');
+var path = require('path');
 
 var paths = {
 	css: './src/css/**/*',
@@ -28,50 +34,77 @@ var paths = {
 	]
 };
 
+function gulp_size_log(title) {
+	return gulp_size({
+		title: title
+	});
+}
+
 gulp.task('bower', function() {
-	return bower();
+	return gulp_bower();
 });
 
 gulp.task('css', function() {
+	var DEST = 'build/public/css';
+	var NAME = 'styles.css';
+	var NAME_MIN = 'styles.min.css';
 	return gulp.src(paths.css)
-		.pipe(less())
-		.pipe(rename('styles.css'))
-		.pipe(gulp.dest('build/public/css'))
-		.pipe(minify_css())
-		.pipe(rename('styles.min.css'))
-		.pipe(gulp.dest('build/public/css'));
+		.pipe(gulp_newer(path.join(DEST, NAME)))
+		.pipe(gulp_less())
+		.pipe(gulp_rename(NAME))
+		.pipe(gulp_size_log(NAME))
+		.pipe(gulp.dest(DEST))
+		.pipe(gulp_minify_css())
+		.pipe(gulp_rename(NAME_MIN))
+		.pipe(gulp_size_log(NAME_MIN))
+		.pipe(gulp.dest(DEST));
 });
 
 gulp.task('jshint', function() {
 	return gulp.src(paths.scripts)
-		.pipe(filter('*.js'))
-		.pipe(jshint())
-		.pipe(jshint.reporter(jshint_stylish))
-		.pipe(jshint.reporter('fail'));
+		.pipe(gulp_filter('*.js'))
+		.pipe(gulp_cached('jshint'))
+		.pipe(gulp_jshint())
+		.pipe(gulp_jshint.reporter(jshint_stylish))
+		.pipe(gulp_jshint.reporter('fail'));
 });
 
 gulp.task('ng', function() {
+	var DEST = 'build/';
+	var NAME = 'templates.js';
 	return gulp.src(paths.views_ng)
-		.pipe(ng_template_cache())
-		.pipe(gulp.dest('build/'));
+		.pipe(gulp_newer(path.join(DEST, NAME)))
+		.pipe(gulp_ng_template())
+		.pipe(gulp_size_log(NAME))
+		.pipe(gulp.dest(DEST));
 });
 
 gulp.task('js', ['bower', 'jshint', 'ng'], function() {
-	// single entry point to browserify
-	var clientify = gulp.src(paths.client_main)
-		.pipe(browserify({
-			insertGlobals: true,
-			debug: true
-		}));
-	// clientify.pipe(ngmin());
-	return es.merge(clientify, gulp.src(paths.client_externals))
-		.pipe(gconcat('bundle.js'))
-		.pipe(gulp.dest('build/public/js'))
-		.pipe(uglify({
-			// mangle: false
-		}))
-		.pipe(rename('bundle.min.js'))
-		.pipe(gulp.dest('build/public/js'));
+	var DEST = 'build/public/js';
+	var NAME = 'bundle.js';
+	var NAME_MIN = 'bundle.min.js';
+	var bundler = browserify(paths.client_main);
+	var bundle_options = {
+		insertGlobals: true,
+		list: true,
+		debug: true
+	};
+	var client_bundle_stream = bundler.bundle(bundle_options)
+		.pipe(vinyl_source_stream(NAME))
+		.pipe(vinyl_buffer());
+	var client_merged_stream = event_stream.merge(
+		client_bundle_stream,
+		gulp.src(paths.client_externals)
+	);
+	return client_merged_stream
+		.pipe(gulp_concat(NAME))
+		.pipe(gulp_size_log(NAME))
+		.pipe(gulp.dest(DEST))
+		.pipe(gulp_cached(NAME))
+		.pipe(gulp_uglify())
+		.pipe(gulp_rename(NAME_MIN))
+		.pipe(gulp_size_log(NAME_MIN))
+		.pipe(gulp.dest(DEST));
 });
 
 
@@ -82,7 +115,7 @@ var nodemon_instance;
 
 gulp.task('serve', ['install'], function() {
 	if (!nodemon_instance) {
-		nodemon_instance = nodemon({
+		nodemon_instance = gulp_nodemon({
 			script: paths.server_main,
 			watch: 'src/__manual_watch__/',
 			ext: '__manual_watch__',
