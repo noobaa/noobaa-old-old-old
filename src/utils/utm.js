ab80a63327c1d6014e0f7712e3edc12960e25044
yuvaldim <yuval.dimnik@gmail.com>
2014-09-29 11:37:15 +0300

    Don't really need the file any longer

--- src/utils/utm.js
+++
@@ -1,4 +0,0 @@
-//refid is NooBaa's custome field we used in the past. We'll see how used in the future.
-//Rest of the fields could be found here: https://support.google.com/analytics/answer/1033867?hl=en
-var utm_tracked_field = ['refid', 'utm_source', 'utm_medium', 'utm_term', 'utm_content', 'utm_campaign'];
-exports.utm_tracked_field = utm_tracked_field;
